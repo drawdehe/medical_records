928eb0b8c2696620975c465c4a7e651afd9c46e2
Philip Schager <mte12psc@student.lu.se>
2020-03-03 11:26:40 +0100

    added patientfile

---
+++ MedicalRecords/src/PatientFile.java
@@ -0,0 +1,43 @@
+
+public class PatientFile {
+
+		private String[] data;
+		
+		public PatientFile(String name, String doctor, String nurse, String division, String text) {
+			data = new String[5];
+			data[0] = name;
+			data[1] = doctor;
+			data[2] = nurse;
+			data[3] = division;
+			data[4] = text;
+		}	
+		
+		public String getPatientName() {
+			return data[0];
+		}
+		
+		public String getDoctorName() {
+			return data[1];
+		}
+		
+		public String getNurseName() {
+			return data[2];
+		}
+		
+		public String getDivision() {
+			return data[3];
+		}
+		
+		public String getMedicalInfo() {
+			return data[4];
+		}
+		
+		public void appendMedicalText(String s) {
+			data[4] = data[4] + " | " + s;
+		}
+		
+		public String toString() {
+			return data[0];
+		}
+
+}
