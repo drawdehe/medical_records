842a881ef45b47b30d6f05532e6a85ea87944132
ed2576sj-s <ed2576sj-s@hacke-9.e.lth.lu.se>
2020-03-03 11:09:41 +0100

    Implemented constructor in Log

--- MedicalRecords/src/Log.java
+++ MedicalRecords/src/Log.java
@@ -1,16 +1,19 @@
 import java.time.LocalDateTime;
 
 public class Log {
-	private String user;
+	private String ssn_user;
+	private String ssn_patient;
+	private String operation;
+	private Boolean permission;
 	private LocalDateTime time;
 	private String text;
-	private String operation;
 	
-	//User should probably not be a string
-	
-	public Log(String user, String operation) {
-		this.user = user;
+	public Log(String ssn_user, String ssn_patient, String operation, Boolean permission, LocalDateTime time) {
+		this.ssn_user = ssn_user;
+		this.ssn_patient = ssn_patient;
 		this.operation = operation;
+		this.permission = false;
+		this.time = time.now();
 	}
 	public void accessed() {
 		if (operation == "read") {
