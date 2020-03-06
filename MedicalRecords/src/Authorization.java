e60cdc196cbb0b3e29f72b034fc0186cd4462f26
ed2576sj-s <ed2576sj-s@beta-9.e.lth.lu.se>
2020-03-06 17:25:04 +0100

    Update

--- MedicalRecords/src/Authorization.java
+++ MedicalRecords/src/Authorization.java
@@ -1,45 +1,7 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-
 public class Authorization {
 
 	public Authorization() {
 		
-	}
-	/*
-	 * isAssociated går in i en map som ska vara sparad för varje patient, om
-	 * patientens namn är Alban skulle mapen heta Alban, while loopen stegar igenom
-	 * varje line med br.readline(), om den hittar en line som är samma subjektets
-	 * namn returneras true, om det aldrig hittas returneras false. varje line i
-	 * file.txt består endast av ett personnummer på en läkare eller sjuksyster som
-	 * är associerad med patienten.
-	 */
-
-	/*
-	 * isAssociated kollar i en patients journal om en doctor eller nurse är
-	 * associerad med patienten. Var fil inleds med information om vilka doctors och
-	 * nurses som är associerade med patienten, samt dess avdelning. Den
-	 * informationen kollas igenom för att se om det angivna namnet "name" matchar.
-	 */
-	public Boolean isAssociated(String employee_id, String ssn_patient) throws FileNotFoundException, IOException {
-		String file = ssn_patient + ".txt";
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		try {
-			String line = br.readLine();
-
-			while (line != null) {
-				if (line.equals(employee_id)) {
-					return true;
-				}
-				line = br.readLine();
-			}
-			return false;
-		} finally {
-			br.close();
-		}
 	}
 	/*
 	 * tar subject strängen vi får från certifikatet och parsar den, hämtar ut
