21c034d9e96b5ccdb22a0c9808ca9d0fbd267e89
ed2576sj-s <ed2576sj-s@beta-9.e.lth.lu.se>
2020-03-06 13:51:15 +0100

    Deleted unused code

--- MedicalRecords/src/Log.java
+++ MedicalRecords/src/Log.java
@@ -1,46 +1,12 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.time.LocalDateTime;
 
 public class Log {
-	//private String ssn_user;
-	//private String ssn_patient;
-	//private String operation;
-	//private Boolean permission;
-	//private LocalDateTime time;
-	//private String text;
 
-//	private BufferedWriter bw;
-//	private FileWriter writer;
-	
-//	public Log() throws IOException {
-//		/*this.ssn_user = ssn_user;
-//		this.ssn_patient = ssn_patient;
-//		this.operation = operation;
-//		this.permission = false;
-//		this.time = time.now();*/
-//		//this.time = time;
-//		FileWriter writer = new FileWriter("log.txt");
-//		bw = new BufferedWriter(writer);
-//	}
 	public Log() {
-		String hej = "hej";
-		//try {
-		//	FileWriter fw = new FileWriter(new File("Log.txt"));
-		//	fw.close();
-		//} catch (IOException e) {
-		//	e.printStackTrace();
-		//}
 
 	}
-	
 	public static void newLogEntry(String ssn_user, String ssn_patient, String operation, Boolean permission) {
 		String access;
 		if(permission) {
@@ -60,12 +26,4 @@ public class Log {
 			e.printStackTrace();
 		}
 	}
-	
-//	public void accessed(String ssn_user, String ssn_patient, String operation, Boolean permission) throws IOException {
-//		LocalDateTime now = LocalDateTime.now();
-//		bw.write("user: " + ssn_user + ", operation: " + operation + ", patient: " + ssn_patient + ", date: " + now);
-//		bw.newLine();
-//	}
-	
-
  }
